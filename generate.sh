2e02e9c375414cf8997ed1034804101993e04dd9
yaiba <4yaiba@gmail.com>
2023-05-31 10:50:12 -0500

    do not generate parse tree listener

--- generate.sh
+++ generate.sh
@@ -10,4 +10,4 @@ if [ ! -f $antlr_bin ]; then
 fi
 
 alias antlr4='java -Xmx500M -cp "./${antlr_bin}:$CLASSPATH" org.antlr.v4.Tool'
-antlr4 -Dlanguage=Go -visitor -package sql_grammar *.g4
+antlr4 -Dlanguage=Go -visitor -no-listener -package sql_grammar *.g4
