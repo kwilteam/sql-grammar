7140d0418660f763150842cb6a6ebfef8ab25d3d
yaiba <4yaiba@gmail.com>
2023-06-14 14:18:08 -0500

    support generate to a output_dir

--- generate.sh
+++ generate.sh
@@ -4,6 +4,7 @@ set -e
 
 target=${1:-Go}
 package=${2:-sqlgrammar}
+output_dir=${3:-.}
 
 antlr_bin=antlr-4.12.0-complete.jar
 
@@ -13,4 +14,4 @@ if [ ! -f $antlr_bin ]; then
 fi
 
 alias antlr4='java -Xmx500M -cp "./${antlr_bin}:$CLASSPATH" org.antlr.v4.Tool'
-antlr4 -Dlanguage="${target}" -visitor -no-listener -package "${package}" *.g4
+antlr4 -Dlanguage="${target}" -visitor -no-listener -package "${package}" -o "${output_dir}" *.g4
